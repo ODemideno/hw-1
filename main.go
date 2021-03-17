ffc50a5676e41a716841c83bc2280491513482cc
ODemideno <dahaka879@gmail.com>
2021-03-17 14:16:42 +0400

    Add a func Planets

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	planets()
+}
+func planets() {
+	planets := [...]string{
+		"Меркурий",
+		"Венера",
+		"Земля",
+		"Марс",
+		"Юпитер",
+		"Сатурн",
+		"Уран",
+		"Нептун",
+	}
+
+	terrestrial := planets[0:4]
+	gasGiants := planets[4:6]
+	iceGiants := planets[6:8]
+	fmt.Println(terrestrial, gasGiants, iceGiants)
 }
